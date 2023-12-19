69a954d14d4d1c5ece95bca23060f39abf01b5ae
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-19 06:39:12 -0500

    Code the main memory class with read, write and get_exec_time methods

--- memory.py
+++ memory.py
@@ -16,3 +16,37 @@ class Memory:
         if output:
             print(f" - {self.name} read: ", end="")
         self.exec_time += self.access_time
+
+    # Output write message and update the process execution
+    # time
+    def write(self, output=True):
+        if output:
+            print(f" - {self.name} write: ", end="")
+        self.exec_time += self.access_time
+
+    # Placeholder method
+    def get_exec_time(self):
+        return 0
+
+
+# Memory class used for the main memory data storage
+class MainMemory(Memory):
+    def __init__(self):
+        Memory.__init__(self, name="Main memory", access_time=30)
+        self.data = [""] * 16
+
+    # Return data from main memory address
+    def read(self, address):
+        data = self.data[address]
+        super().read()
+        return data
+
+    # Write data to main memory address
+    def write(self, address, data):
+        self.data[address] = data
+        super().write()
+
+    # Return the total execution time
+    def get_exec_time(self):
+        return self.exec_time
+
